ed79db29c1a5b6908e2542b8562ef218faf17db3
slaier <30682486+Slaier@users.noreply.github.com>
2020-08-17 00:01:33 +0800

    add 腾讯云社区

--- generate_block_list.fish
+++ generate_block_list.fish
@@ -5,6 +5,7 @@ cat block_domain_list.txt | sort | uniq > temp
 mv temp block_domain_list.txt
 
 # generate ublacklist
-cat uBlacklist_match_patterns.txt >> temp
+cat uBlacklist_match_patterns.txt | sort | uniq >> temp
+cp temp uBlacklist_match_patterns.txt
 cat block_domain_list.txt | parallel -k echo  '\*://'{}'/\*' >> temp
 mv temp uBlacklist_subscription.txt
