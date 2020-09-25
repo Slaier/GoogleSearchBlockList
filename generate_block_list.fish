dffac8da12f122bb762fa72cd4282c20e336e71c
slaier <30682486+Slaier@users.noreply.github.com>
2020-09-25 22:11:53 +0800

    weekly update 2020/09/25

--- generate_block_list.fish
+++ generate_block_list.fish
@@ -1,11 +1,11 @@
 #!/usr/bin/env fish
 
 # sort ban list
-cat block_domain_list.txt | sort | uniq > temp
+cat block_domain_list.txt | sort -s | uniq > temp
 mv temp block_domain_list.txt
 
 # generate ublacklist
-cat uBlacklist_match_patterns.txt | sort | uniq >> temp
+cat uBlacklist_match_patterns.txt | sort -s | uniq >> temp
 cp temp uBlacklist_match_patterns.txt
 awk '{printf "*://%s/*\n",$0}' block_domain_list.txt Blocklist/lunzi Blocklist/BLOCKLIST >> temp
 mv temp uBlacklist_subscription.txt
