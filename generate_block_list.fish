742e9f15e1457de5378808966669820aff554ea8
slaier <30682486+Slaier@users.noreply.github.com>
2021-02-21 17:58:42 +0800

    update@2021-02-21

--- generate_block_list.fish
+++ generate_block_list.fish
@@ -10,15 +10,19 @@ end
 
 # generate ublacklist
 cat match_patterns.txt Google-Chinese-Results-Blocklist/uBlacklist_match_patterns.txt > uBlacklist_match_patterns.txt
-sort_list uBlacklist_match_patterns.txt
 
 awk '{printf "*://%s/*\n",$0}' perma_ban_list.txt block_list.txt Blocklist/lunzi Blocklist/BLOCKLIST > uBlacklist_subscription.txt
 cat Google-Chinese-Results-Blocklist/uBlacklist_subscription.txt >> uBlacklist_subscription.txt
-sort_list uBlacklist_subscription.txt
 
 # generate GHHbD
 cat perma_ban_list.txt Google-Chinese-Results-Blocklist/GHHbD_perma_ban_list.txt Blocklist/lunzi Blocklist/BLOCKLIST > GHHbD_perma_ban_list.txt
-sort_list GHHbD_perma_ban_list.txt
-
 cat block_list.txt > GHHbD_block_list.txt
+
+# sort list
+sort_list uBlacklist_match_patterns.txt
+sort_list uBlacklist_subscription.txt
+sort_list GHHbD_perma_ban_list.txt
 sort_list GHHbD_block_list.txt
+sort_list block_list.txt
+sort_list perma_ban_list.txt
+sort_list match_patterns.txt
